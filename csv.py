28d0b9bc2e2f8534ab3da082cef0cc446c257610
gari <garillka@gmail.com>
2017-01-28 16:38:03 +0300

    Fix csv date range

--- csv.py
+++ csv.py
@@ -2,37 +2,67 @@
 
 import sys
 import json
-import re
 from datetime import datetime
 from datetime import timedelta
 
-# Skip the configuration settings.
+DATEFORMAT = '%Y%m%dT%H%M%SZ'
+
+# Extract the configuration settings.
+header = 1
+configuration = dict()
+body = ''
 for line in sys.stdin:
-    # print(line)
-    if re.match("temp.report.tags", line):
-        print(line)
-    if line == '\n':
-        break;
+    if header:
+        if line == '\n':
+            header = 0
+        else:
+            fields = line.strip().split(': ', 2)
+            if len(fields) == 2:
+                configuration[fields[0]] = fields[1]
+            else:
+                configuration[fields[0]] = ''
+    else:
+        body += line
+
+# Sum the second tracked by tag.
+totals = dict()
+j = json.loads(body)
+for object in j:
+    start = datetime.strptime(object['start'], DATEFORMAT)
+
+    if 'end' in object:
+        end = datetime.strptime(object['end'], DATEFORMAT)
+    else:
+        end = datetime.utcnow()
+
+    tracked = end - start
+    # print(object['tags'])
+    for tag in object['tags']:
+        # print(tag)
+        if tag in totals:
+            totals[tag] += tracked
+        else:
+            totals[tag] = tracked
 
 # Extract the JSON.
-doc = ''
-for line in sys.stdin:
-    doc += line
+# doc = ''
+# for line in sys.stdin:
+#     doc += line
 
 total_active_time = 0
 
-j = json.loads(doc)
+j = json.loads(body)
 for object in j:
     fix = timedelta(hours=3)
     start = object['start']
     if 'end' in object:
         end = object['end']
-        end_time = datetime.strptime(end, '%Y%m%dT%H%M%SZ')
+        end_time = datetime.strptime(end, DATEFORMAT)
         end_time += fix
     else:
-        end = datetime.now().strftime("%Y%m%dT%H%M%SZ")
-        end_time = datetime.strptime(end, '%Y%m%dT%H%M%SZ')
-    start_time = datetime.strptime(start, '%Y%m%dT%H%M%SZ')
+        end = datetime.now().strftime(DATEFORMAT)
+        end_time = datetime.strptime(end, DATEFORMAT)
+    start_time = datetime.strptime(start, DATEFORMAT)
     start_time += fix
     line = '"%s",' % start_time
     line += '"%s"' % end_time
